731a4bba98685dfc3b62decb3184f606e17553df
Marcello de Sales <Marcello_deSales@intuit.com>
2016-01-21 06:32:29 -0800

    Initial commit: Dockerfile, system local config, README
    *	new file:   Dockerfile
    - Initial dockerfile
    
    *	new file:   etc-system-local/*
    - Default syslog settings for collecting logs from /var/log/messages
    - Contains 2 tokens: _HOSTNAME_ and _INDEX_ that can be replaced with
      $FORWARDER_HOSTNAME and $SPLUNK_INDEX
    
    *	new file:   patch-entrypoint.sh
    - Patched entrypoint, copied from https://github.com/outcoldman/docker-splunk/blob/master/universalforwarder/entrypoint.sh that is used to replace the tokens.
    
    *	new file:   splunk.yml
    - Docker-compose file describing a possible execution.

---
+++ patch-entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Figure out for logging
+if [ "$1" = 'splunk' ]; then
+  shift
+  sudo -HEu ${SPLUNK_USER} ${SPLUNK_HOME}/bin/splunk "$@"
+elif [ "$1" = 'start-service' ]; then
+  # If these files are different override etc folder (possible that this is upgrade or first start cases)
+  # Also override ownership of these files to splunk:splunk
+  if ! $(cmp --silent /var/opt/splunk/etc/splunk.version ${SPLUNK_HOME}/etc/splunk.version); then
+    cp -fR /var/opt/splunk/etc ${SPLUNK_HOME}
+    chown -R ${SPLUNK_USER}:${SPLUNK_GROUP} $SPLUNK_HOME/etc
+    chown -R ${SPLUNK_USER}:${SPLUNK_GROUP} $SPLUNK_HOME/var
+  fi
+
+  # Replacing the values from the configuration
+  cp -R /tmp/splunk /tmp/splunk-for-host
+  cd /tmp/splunk-for-host
+  sed -i "s/_HOSTNAME_/$FORWARD_HOSTNAME/g" *
+  sed -i "s/_INDEX_/$SPLUNK_INDEX/g" *
+  cp -rf /tmp/splunk-for-host/* /opt/splunk/etc/system/local/
+
+  sudo -HEu ${SPLUNK_USER} ${SPLUNK_HOME}/bin/splunk start --accept-license --answer-yes --no-prompt
+  sudo -HEu ${SPLUNK_USER} tail -f ${SPLUNK_HOME}/var/log/splunk/splunkd_stderr.log
+else
+  "$@"
+fi
